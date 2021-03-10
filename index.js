df82d29126c26ecd8ae81323beacd225d5541821
afurgeri <alejandro.furgeri@sondeos.com.ar>
2021-03-10 11:33:25 -0300

    completed

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const challengeFunction = function(param){
-    /* YOUR CODE HERE */
-    let result
+const challengeFunction = function(cell1, cell2){
 
-    return result
+    return isWhiteColor(cell1) === isWhiteColor(cell2)
 }
 
+function isWhiteColor(cell) { 
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    let position = cell.split('')
+    return (!isPar(letters.indexOf(position[0]) + 1) && isPar(position[1])) || (isPar(letters.indexOf(position[0]) + 1) && !isPar(position[1]))
+}
+
+function isPar(number){
+    return number % 2 == 0
+}
 module.exports = challengeFunction
